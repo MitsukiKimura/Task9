dcb1d16cda8f6903d5810da219c13c93a1aa50db
木村 光希 <180012@jc-21.jp>
2021-06-14 12:46:19 +0900

    owa

---
+++ src/main/java/jp/te4a/spring/boot/mybootapp9/MyBootApp9Application.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.mybootapp9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootApp9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApp9Application.class, args);
+    }
+
+}
